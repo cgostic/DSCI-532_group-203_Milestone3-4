296e46083039e1fda2af09e78ded989b6e23c4f0
cgostic <42299566+cgostic@users.noreply.github.com>
2019-12-12 17:05:49 -0800

    Update init.R

--- init.R
+++ init.R
@@ -14,52 +14,52 @@ install.packages("remotes")
 # installs Rcpp, rlang, BH
 install.packages("later")
 
-# install.packages("https://cran.r-project.org/src/contrib/jsonlite_1.6.tar.gz", type="source", repos=NULL)
-# install.packages("rjson")
-# install.packages("listenv")
-# install.packages("anytime")
-# install.packages("readr")
-# install.packages("heatmaply")
+install.packages("https://cran.r-project.org/src/contrib/jsonlite_1.6.tar.gz", type="source", repos=NULL)
+install.packages("rjson")
+install.packages("listenv")
+install.packages("anytime")
+install.packages("readr")
+install.packages("heatmaply")
 
-# # installs magrittr, promises, R6
-# remotes::install_version("httpuv", version = "1.4.5.1", repos = "http://cloud.r-project.org", upgrade="never")
+# installs magrittr, promises, R6
+remotes::install_version("httpuv", version = "1.4.5.1", repos = "http://cloud.r-project.org", upgrade="never")
 
-# # installs crayon, digest, htmltools, mime, sourcetools, xtable
-# remotes::install_version("shiny", version = "1.2.0", repos = "http://cloud.r-project.org", upgrade="never")
+# installs crayon, digest, htmltools, mime, sourcetools, xtable
+remotes::install_version("shiny", version = "1.2.0", repos = "http://cloud.r-project.org", upgrade="never")
 
-# # installs askpass, assertthat, base64enc, cli, colorspace, crosstalk, curl, data.table, dplyr, fansi, ggplot2, glue, gtable, hexbin, htmlwidgets, httr, labeling, lattice, lazyeval, mgcv, munsell, nlme, openssl, pillar, pkgconfig, plogr, plyr, purrr, RColorBrewer, reshape2, scales, stringi, stringr, sys, tibble, tidyr, tidyselect, utf8, viridisLite, withr, yaml
-# remotes::install_version("plotly", version = "4.9.1", repos = "http://cloud.r-project.org", upgrade="never")
+# installs askpass, assertthat, base64enc, cli, colorspace, crosstalk, curl, data.table, dplyr, fansi, ggplot2, glue, gtable, hexbin, htmlwidgets, httr, labeling, lattice, lazyeval, mgcv, munsell, nlme, openssl, pillar, pkgconfig, plogr, plyr, purrr, RColorBrewer, reshape2, scales, stringi, stringr, sys, tibble, tidyr, tidyselect, utf8, viridisLite, withr, yaml
+remotes::install_version("plotly", version = "4.9.1", repos = "http://cloud.r-project.org", upgrade="never")
 
-# install.packages("https://cloud.r-project.org/src/contrib/assertthat_0.2.1.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/xml2_1.2.2.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/triebeard_0.3.0.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/Archive/urltools/urltools_1.7.2.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/jsonlite_1.6.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/webutils_1.0.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/brotli_1.2.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/reqres_0.2.3.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/uuid_0.1-2.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/base64enc_0.1-3.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/codetools_0.2-16.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/globals_0.12.5.tar.gz", type="source", repos=NULL)
-# install.packages("https://cloud.r-project.org/src/contrib/Archive/future/future_1.11.1.1.tar.gz", type="source", repos=NULL)
-# # gghighlight
-# install.packages("https://cran.r-project.org/src/contrib/gghighlight_0.1.0.tar.gz", type="source", repos=NULL)
-# # sf
-# install.packages("https://cran.r-project.org/src/contrib/Archive/sf/sf_0.3-1.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/assertthat_0.2.1.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/xml2_1.2.2.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/triebeard_0.3.0.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/Archive/urltools/urltools_1.7.2.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/jsonlite_1.6.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/webutils_1.0.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/brotli_1.2.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/reqres_0.2.3.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/uuid_0.1-2.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/base64enc_0.1-3.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/codetools_0.2-16.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/globals_0.12.5.tar.gz", type="source", repos=NULL)
+install.packages("https://cloud.r-project.org/src/contrib/Archive/future/future_1.11.1.1.tar.gz", type="source", repos=NULL)
+# gghighlight
+install.packages("https://cran.r-project.org/src/contrib/gghighlight_0.1.0.tar.gz", type="source", repos=NULL)
+# sf
+install.packages("https://cran.r-project.org/src/contrib/Archive/sf/sf_0.3-1.tar.gz", type="source", repos=NULL)
 # rgdal
 install.packages("https://cran.r-project.org/src/contrib/Archive/rgdal/rgdal_1.4-4.tar.gz", type = 'source', repos = NULL)
-# # # rgeos
-# # install.packages("https://cran.r-project.org/src/contrib/rgeos_0.5-2.tar.gz", type = 'source', repos = NULL)
-# # ggrepel
-# install.packages("https://cran.r-project.org/src/contrib/ggrepel_0.8.1.tar.gz", type = 'source', repos = NULL)
-# # DBI
-# install.packages("https://cran.r-project.org/src/contrib/DBI_1.0.0.tar.gz", type = 'source', repos = NULL)
-# # units
-# install.packages("https://cran.r-project.org/src/contrib/units_0.6-5.tar.gz", type = 'source', repos = NULL)
-# # sp
-# install.packages("https://cran.r-project.org/src/contrib/sp_1.3-2.tar.gz", type = 'source', repos = NULL)
-# # geojsonio
+# # rgeos
+# install.packages("https://cran.r-project.org/src/contrib/rgeos_0.5-2.tar.gz", type = 'source', repos = NULL)
+# ggrepel
+install.packages("https://cran.r-project.org/src/contrib/ggrepel_0.8.1.tar.gz", type = 'source', repos = NULL)
+# DBI
+install.packages("https://cran.r-project.org/src/contrib/DBI_1.0.0.tar.gz", type = 'source', repos = NULL)
+# units
+install.packages("https://cran.r-project.org/src/contrib/units_0.6-5.tar.gz", type = 'source', repos = NULL)
+# sp
+install.packages("https://cran.r-project.org/src/contrib/sp_1.3-2.tar.gz", type = 'source', repos = NULL)
+# geojsonio
 
 
 
